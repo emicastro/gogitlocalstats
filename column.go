a02e98ca295e0fc3f7d479f05152ffc777756527
Emilio Castro <emilio.castro@mercadolibre.com>
2020-07-13 23:08:56 -0300

    Adding functions to print cells

--- column.go
+++
@@ -1,3 +0,0 @@
-package main
-
-type column []int
--- stats.go
+++ stats.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"time"
 
 	"github.com/go-git/go-git"
-	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/plumbing/object"
 )
 
-const daysInLastSixMoths = 183
 const outOfRange = 99999
+const daysInLastSixMoths = 183
+const weeksInLastSixMonths = 26
+
+type column []int
 
 // stats calculates and prints the stats.
 func stats(email string) {
 	commits := processRepositories(email)
-	printCommitsStats(commits)
+	printCommitStats(commits)
 }
 
 // processRepositories given a user email,
@@ -163,4 +167,101 @@ func buildCols(keys []int, commits map[int]int) map[int]column {
 	}
 
 	return cols
+}
+
+// printCells prints the cells of the graph
+func printCells(cols map[int]column) {
+	printMonths()
+	for j := 6; j >= 0; j-- {
+		for i := weeksInLastSixMonths + 1; i >= 0; i-- {
+			if i == weeksInLastSixMonths+1 {
+				printDayCol(j)
+			}
+			if col, ok := cols[i]; ok {
+				// special case today
+				if i == 0 && j == calcOffset()-1 {
+					printCell(col[j], true)
+					continue
+				} else {
+					if len(col) > j {
+						printCell(col[j], false)
+						continue
+					}
+				}
+			}
+			printCell(0, false)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+// printMonths prints the month names in the first line,
+// determining when the month changed between switching weeks
+func printMonths() {
+	week := getBeginningOfDay(time.Now()).Add(-(daysInLastSixMoths * time.Hour * 24))
+	month := week.Month()
+	fmt.Printf("         ")
+	for {
+		if week.Month() != month {
+			fmt.Printf("%s ", week.Month().String()[:3])
+			month = week.Month()
+		} else {
+			fmt.Printf("         ")
+		}
+
+		week = week.Add(7 * time.Hour * 24)
+		if week.After(time.Now()) {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+// printDayCol given the day number (0 is Sunday) prints the day name,
+// alternating the rows (prints just 2, 4, 6)
+func printDayCol(day int) {
+	out := "    "
+	switch day {
+	case 1:
+		out = " Mon "
+	case 3:
+		out = " Wed "
+	case 5:
+		out = " Fri "
+	}
+
+	fmt.Printf(out)
+}
+
+// printCell given a cell value prints it with a different format
+// based on the value amount, and on the `today` flag.
+func printCell(val int, today bool) {
+	escape := "\033[0;37;30m"
+	switch {
+	case val > 0 && val < 5:
+		escape = "\033[1;30;47m"
+	case val >= 5 && val < 10:
+		escape = "\033[1;30;43m"
+	case val >= 10:
+		escape = "\033[1;30;42m"
+	}
+
+	if today {
+		escape = "\033[1;37;45m"
+	}
+
+	if val == 0 {
+		fmt.Printf(escape + "  - " + "\033[0m")
+		return
+	}
+
+	str := "  %d "
+	switch {
+	case val >= 10:
+		str = " %d "
+	case val >= 100:
+		str = "%d "
+	}
+
+	fmt.Printf(escape+str+"\033[0m", val)
 }
