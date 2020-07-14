09381d67258c1fde47aa5c17a9ca46a9f7facc0d
Emilio Castro <emilio.castro@mercadolibre.com>
2020-07-13 21:24:37 -0300

    WIP | Added fuctions to calculate dates passed since a commit's date

--- stats.go
+++ stats.go
@@ -1,6 +1,98 @@
 package main
 
-// stats generates a niche graph of git contributions
+import (
+	"time"
+
+	"github.com/go-git/go-git"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+const daysInLastSixMoths = 183
+const outOfRange = 99999
+
+// stats calculates and prints the stats.
 func stats(email string) {
-	print("stats")
+	commits := processRepositories(email)
+	printCommitsStats(commits)
+}
+
+// processRepositories given a user email,
+// returns the commits made in the last 6 months.
+func processRepositories(email string) map[int]int {
+	filePath := getDotFilePath()
+	repos := parseFileLinesToSlice(filePath)
+	daysInMap := daysInLastSixMoths
+
+	commits := make(map[int]int, daysInMap)
+	for i := daysInMap; i > 0; i-- {
+		commits[i] = 0
+	}
+
+	for _, path := range repos {
+		commits = fillCommits(email, path, commits)
+	}
+
+	return commits
+}
+
+// fillCommits given a repository found in `path`, gets the commits and
+// puts them in the `commits` map, returning it when completed
+func fillCommits(email string, path string, commits map[int]int) map[int]int {
+	// instantiate a git repo object from path
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		panic(err)
+	}
+	//get the HEAD reference
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+	// get the commits history stating from HEAD
+	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+	// iterate thought the commits
+	offset := calcOffset()
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := countDaysSinceDate(c.Author.When) + offset
+
+		if c.Author.Email != email {
+			return nil
+		}
+
+		if daysAgo != outOfRange {
+			commits[daysAgo]++
+		}
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return commits
+}
+
+// getBeginningOfDay given a time.Time calculates the start time of that day
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return startOfDay
+}
+
+// countDaysSinceDate counts how many days passed since the passed `date`.
+func countDaysSinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMoths {
+			return outOfRange
+		}
+	}
+
+	return days
 }
